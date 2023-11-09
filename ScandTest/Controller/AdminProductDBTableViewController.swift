101fbe28644440f52b1d1cb027f884f4e05dde12
alex <alex.vasilevski007@gmail.com>
2023-11-09 11:16:02 +0300

    refactor: delete all coomented mess

--- ScandTest/Controller/AdminProductDBTableViewController.swift
+++ ScandTest/Controller/AdminProductDBTableViewController.swift
@@ -45,14 +45,6 @@ class AdminProductDBTableViewController: UITableViewController {
         self.tableView.addSubview(self.refresh)
         self.refresh.beginRefreshing()
         self.fetchProducts()
-        
-        
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     @objc func fetchProducts() {
@@ -66,12 +58,10 @@ class AdminProductDBTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return productsDisplayed.count + 1
     }
 
@@ -132,7 +122,7 @@ class AdminProductDBTableViewController: UITableViewController {
         }
     }
     
-    //MARK: this is working only with default cell.accessoryType = .detailButton , i wanted to ese chekmark, but its'not calling this((
+    //MARK: this is working only with default cell.accessoryType = .detailButton , i wanted to use chekmark first, but its'not calling this((
 //    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
 //        print("accesory tapped")
 //        let tappedCell = tableView.cellForRow(at: indexPath) as! AdminProductCell
@@ -149,53 +139,6 @@ class AdminProductDBTableViewController: UITableViewController {
 //            }
 //        }
 //    }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 
